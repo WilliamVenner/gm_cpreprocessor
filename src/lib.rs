d6a85171dd671d249bf9cabad613d10af2ba79de
William Venner <william@venner.io>
2022-03-02 20:44:27 +0000

    Remove the detour on gmod13_close

--- src/lib.rs
+++ src/lib.rs
@@ -48,25 +48,33 @@ extern "cdecl" fn RunStringEx_Detour(this: *mut c_void, path: *const c_char, unk
 
 #[gmod13_open]
 unsafe fn gmod13_open(lua: gmod::lua::State) {
-	if cpp::can_preprocess() {
-		let (_lib, _path) = open_library_srv!("lua_shared").expect("Failed to find lua_shared!");
+	if !cpp::can_preprocess() {
+		return
+	};
 
-		let RunStringEx: RunStringEx = find_gmod_signature!((_lib, _path) -> {
+	let (_lib, _path) = open_library_srv!("lua_shared").expect("Failed to find lua_shared!");
 
-			win64_x86_64: [@SIG = "40 55 53 56 57 41 54 41 56 41 57 48 8D AC 24 ? ? ? ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 49 8B F1 4D 8B F8 4C 8B F2 48 8B F9 4D 85 C9 0F 84"],
-			win32_x86_64: [@SIG = "55 8B EC 81 EC ? ? ? ? 56 57 8B 7D 10 8B F1 85 FF 0F 84 ? ? ? ? 57 E8 ? ? ? ? 83 C4 04 83 F8 01 0F 8C ? ? ? ? 80 3F 1B 75 35 8B 06 6A 1B 68 ? ? ? ? 56 FF 90 ? ? ? ?"],
+	let RunStringEx: RunStringEx = find_gmod_signature!((_lib, _path) -> {
 
-			linux64_x86_64: [@SIG = "55 48 89 E5 41 57 41 56 41 55 49 89 F5 41 54 53 48 89 FB 48 81 EC ? ? ? ? 44 89 85 ? ? ? ? 8B 45 18 44 89 8D ? ? ? ? 44 8B 75 10 89 85"],
-			linux32_x86_64: [@SIG = "55 89 E5 57 56 53 81 EC ? ? ? ? 8B 45 18 8B 55 14 8B 5D 08 8B 7D 0C 89 85 ? ? ? ? 8B 45 1C 8B 75 10 89 85 ? ? ? ? 8B 45 20 89 85 ? ? ? ? 8B 45 24 89 85 ? ? ? ? 65 A1 ? ? ? ? 89 45 E4 31 C0 85 D2 0F 84 ? ? ? ? 89 14 24 89 95"],
+		win64_x86_64: [@SIG = "40 55 53 56 57 41 54 41 56 41 57 48 8D AC 24 ? ? ? ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 49 8B F1 4D 8B F8 4C 8B F2 48 8B F9 4D 85 C9 0F 84"],
+		win32_x86_64: [@SIG = "55 8B EC 81 EC ? ? ? ? 56 57 8B 7D 10 8B F1 85 FF 0F 84 ? ? ? ? 57 E8 ? ? ? ? 83 C4 04 83 F8 01 0F 8C ? ? ? ? 80 3F 1B 75 35 8B 06 6A 1B 68 ? ? ? ? 56 FF 90 ? ? ? ?"],
 
-			win32: [@SIG = "55 8B EC 8B 55 10 81 EC ? ? ? ? 56 8B F1 57 85 D2 0F 84 ? ? ? ? 8B CA 8D 79 01 8D 49 00 8A 01 41 84 C0 75 F9 2B CF 83 F9 01 0F 8C ? ? ? ? 80 3A 1B 75 35"],
-			linux32: [@SIG = "55 89 E5 57 56 53 81 EC ? ? ? ? 8B 45 18 8B 55 14 8B 5D 08 8B 7D 0C 89 85 ? ? ? ? 8B 45 1C 8B 75 10 89 85 ? ? ? ? 8B 45 20 89 85 ? ? ? ? 8B 45 24 89 85 ? ? ? ? 65 A1 ? ? ? ? 89 45 E4 31 C0 85 D2 0F 84 ? ? ? ? 89 14 24 89 95 ? ? ? ?"],
+		linux64_x86_64: [@SIG = "55 48 89 E5 41 57 41 56 41 55 49 89 F5 41 54 53 48 89 FB 48 81 EC ? ? ? ? 44 89 85 ? ? ? ? 8B 45 18 44 89 8D ? ? ? ? 44 8B 75 10 89 85"],
+		linux32_x86_64: [@SIG = "55 89 E5 57 56 53 81 EC ? ? ? ? 8B 45 18 8B 55 14 8B 5D 08 8B 7D 0C 89 85 ? ? ? ? 8B 45 1C 8B 75 10 89 85 ? ? ? ? 8B 45 20 89 85 ? ? ? ? 8B 45 24 89 85 ? ? ? ? 65 A1 ? ? ? ? 89 45 E4 31 C0 85 D2 0F 84 ? ? ? ? 89 14 24 89 95"],
 
-		}).expect("Failed to find CLuaInterface::RunStringEx");
+		win32: [@SIG = "55 8B EC 8B 55 10 81 EC ? ? ? ? 56 8B F1 57 85 D2 0F 84 ? ? ? ? 8B CA 8D 79 01 8D 49 00 8A 01 41 84 C0 75 F9 2B CF 83 F9 01 0F 8C ? ? ? ? 80 3A 1B 75 35"],
+		linux32: [@SIG = "55 89 E5 57 56 53 81 EC ? ? ? ? 8B 45 18 8B 55 14 8B 5D 08 8B 7D 0C 89 85 ? ? ? ? 8B 45 1C 8B 75 10 89 85 ? ? ? ? 8B 45 20 89 85 ? ? ? ? 8B 45 24 89 85 ? ? ? ? 65 A1 ? ? ? ? 89 45 E4 31 C0 85 D2 0F 84 ? ? ? ? 89 14 24 89 95 ? ? ? ?"],
 
-		let detour = gmod::detour::GenericDetour::new::<RunStringEx>(RunStringEx, RunStringEx_Detour).expect("Failed to detour CLuaInterface::RunStringEx");
-		detour.enable().expect("Failed to enable CLuaInterface::RunStringEx detour");
+	}).expect("Failed to find CLuaInterface::RunStringEx");
 
-		DETOUR = Some(detour);
-	}
+	let detour = gmod::detour::GenericDetour::new::<RunStringEx>(RunStringEx, RunStringEx_Detour).expect("Failed to detour CLuaInterface::RunStringEx");
+	detour.enable().expect("Failed to enable CLuaInterface::RunStringEx detour");
+
+	DETOUR = Some(detour);
+}
+
+#[gmod13_close]
+unsafe fn gmod13_close(_: gmod::lua::State) {
+	// Remove the detour
+	DETOUR.take();
 }
